2fc4b57a567db65ec8fa3ab03fcb16b41599eeef
ZhenyuanLau <zhenyuanlau@icloud.com>
2018-08-07 21:09:10 +0800

    Ehancement naming

--- calculator/features/step_definitions/calculator_steps.rb
+++ calculator/features/step_definitions/calculator_steps.rb
@@ -1,11 +1,11 @@
-Given("the input {string}") do |string|
-  @input = string
+Given("the input {string}") do |input|
+  @input = input
 end
 
 When("the calculator is run") do
-  @output = `ruby calc.rb #{@input}`
+  @output = `ruby calculator.rb #{@input}`
 end
 
-Then("the output should be {string}") do |string|
-  expect(@output).to eq string
+Then("the output should be {string}") do |output|
+  expect(@output).to eq output
 end
